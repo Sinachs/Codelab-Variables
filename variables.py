c7c366dd273e9442a53bb5b210a70a64d2fab457
sinachs <sinaychacin@gmail.com>
2024-01-25 00:38:40 -0400

    first commit

---
+++ variables.py
@@ -0,0 +1,19 @@
+# 3. Define una variable de cada tipo primitivo
+entero = 24
+flotante = 2.4444
+cadena = "Hola, mundo"
+booleano = True
+
+# 4. Concatena la cadena con las otras variables aplicando la conversión correcta y guarda el resultado en una variable
+cadena_concatenada = cadena + str(entero) + str(flotante) + str(booleano)
+
+# 6. Aplica la fórmula de la suma de los primeros n números pares (investigar), tomando como n la variable de tipo entero y almacena el resultado en una variable.
+n = entero
+suma_pares = n * (n + 1)
+
+# 7. Imprime los resultados de las tareas anteriores
+print("Variable entero:", entero)
+print("Variable flotante:", flotante)
+print("Variable cadena:", cadena)
+print("Variable booleano:", booleano)
+print("Cadena concatenada:", cadena_concatenada)
